62ea63c54be93ffb38312b63507fea5a043d711d
SandeepChebolu <133785758+SandeepChebolu@users.noreply.github.com>
2023-05-25 20:03:52 +0530

    Delete CV_CRMD_ORDERADM_I.sql

--- models/staging/CV_CRMD_ORDERADM_I.sql
+++
@@ -1,9 +0,0 @@
-with
-    projection_1 as (
-        select sysid, client, parent, number_int, guid, object_type
-
-        from {{ source('sample_poc', 't_tab_crmd_orderadm_i') }}
-
-    )
-select *
-from projection_1
